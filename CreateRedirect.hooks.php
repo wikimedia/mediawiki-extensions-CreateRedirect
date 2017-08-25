591a406459feda5ed1b74319a0767d9c6c8e03a4
Reception123 <utilizator.receptie123@gmail.com>
2017-08-25 08:21:58 +0200

    Convert CreateRedirect to use extension registration
    Bug: T174131
    Change-Id: I697d2973fbed517801213827fca69472faf1fbfe

---
+++ CreateRedirect.hooks.php
@@ -0,0 +1,30 @@
+<?php
+	/**
+	* Adds a shortcut link pointing to Special:CreateRedirect to the "toolbox" menu.
+	* If applicable, also adds a reference to the current title as a GET param.
+	*
+	* @return Boolean: true
+	*/
+class CreateRedirectHooks {
+
+	public static function createRedirect_AddToolboxLink( &$tpl ) {
+		global $wgRequest;
+
+		// 1. Determine whether to actually add the link at all.
+		// There are certain cases, e.g. in the edit dialog, in a special page,
+		// where it's inappropriate for the link to appear.
+		// 2. Check the title. Is it a "Special:" page? Don't display the link.
+		$action = $wgRequest->getText( 'action', 'view' );
+		$title = $tpl->getSkin()->getTitle();
+
+		if( $action != 'view' && $action != 'purge' && !$title->isSpecialPage() ) {
+			return true;
+		}
+
+		// 3. Add the link!
+		$href = SpecialPage::getTitleFor( 'CreateRedirect', $title->getPrefixedText() )->getLocalURL();
+		echo Html::rawElement( 'li', null, Html::element( 'a', array( 'href' => $href ), wfMessage( 'createredirect' )->text() ) );
+
+		return true;
+	}
+}
